8f1a0b9cb2f9cc7d4b5981b290babd5cb1f3f0f5
Yanina Nataliya <n_yanina@ural-press.ru>
2024-12-26 12:30:38 +0300

    v. 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        NetUtils netUtils = new NetUtils();
+
+        List<Stock> stocks = netUtils.getStocks();
+
+        stocks.sort(new Comparator<Stock>() {
+            @Override
+            public int compare(Stock o1, Stock o2) {
+                Float val1 = new Float(o1.getValue());
+                Float val2 = new Float(o2.getValue());
+                return val1.compareTo(val2);
+            }
+        });
+
+        System.out.println("Самая дешевая акция была на " +
+                stocks.get(0).getDate() + " со стоимостью " +
+                stocks.get(0).getValue());
+
+        System.out.println("Самая дорогая акция была на " +
+                stocks.get(stocks.size() - 1).getDate() + " со стоимостью " +
+                stocks.get(stocks.size() - 1).getValue());
     }
 }
--- src/NetUtils.java
+++ src/NetUtils.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class NetUtils {
+    public List<Stock> getStocks() throws IOException {
+        String url = "https://raw.githubusercontent.com/productstar-team/javaTwo/master/resources/monthly_IBM.csv";
+        try (BufferedInputStream is = new BufferedInputStream(new URL(url).openStream())) {
+            Scanner scanner = new Scanner(is).useDelimiter("\\A");
+            String result = scanner.hasNext() ? scanner.next() : "";
+            return convertCsvToStocks(result);
+        } catch (Exception e){
+            System.out.println("Не смогли получить данные из API");
+        }
+        return Collections.emptyList();
+    }
+
+    private List<Stock> convertCsvToStocks(String inputData) {
+        List<Stock> stocks = new ArrayList<>();
+        String[] lines = inputData.split("\n");
+        for (int i = 1; i < lines.length; i++){
+            stocks.add(convertLineToStock(lines[i]));
+        }
+        return stocks;
+    }
+
+    private Stock convertLineToStock(String line) {
+        String[] tokens = line.split(",");
+        return new Stock(
+                tokens[0],
+                Float.parseFloat(tokens[4])
+        );
+    }
 }
